83e2b85a79a1a0abfaf52ba9787825ba12d68e85
Bartosz Lipinski <bartek@airnauts.com>
2016-05-18 10:06:37 +0200

    Project update

--- sample/src/main/java/com/bartoszlipinski/streetviewprobe/sample/activity/MainActivity.java
+++ sample/src/main/java/com/bartoszlipinski/streetviewprobe/sample/activity/MainActivity.java
@@ -1,12 +1,12 @@
-/**
+/*
  * Copyright 2015 Bartosz Lipinski
- * <p/>
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p/>
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p/>
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -29,17 +29,17 @@ import com.bartoszlipinski.streetviewprobe.sample.utils.RoundedCornersTransforma
 import com.squareup.picasso.Picasso;
 
 public class MainActivity extends Activity {
-//    public static final double LAT = 40.720032;
+    //    public static final double LAT = 40.720032;
 //    public static final double LAT = 73.67868;
 //    public static final double LON = -73.988354;
     public static final double LAT = 41.085741;
     public static final double LON = -115.192323;
 
-    private TextView mTextLat;
-    private TextView mTextLon;
-    private TextView mTextAvailability;
-    private ImageView mStreetViewImage;
-    private ImageView mDotImage;
+    private TextView textLat;
+    private TextView textLon;
+    private TextView textAvailability;
+    private ImageView streetViewImage;
+    private ImageView dotImage;
 
     @SuppressLint("NewApi")
     @Override
@@ -49,17 +49,17 @@ public class MainActivity extends Activity {
 
         setupViews();
 
-        mStreetViewImage.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+        streetViewImage.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
 
             @Override
             public void onGlobalLayout() {
-                int width = mStreetViewImage.getWidth();
-                int height = mStreetViewImage.getHeight();
+                int width = streetViewImage.getWidth();
+                int height = streetViewImage.getHeight();
                 if (width > 0 && height > 0) {
                     if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN) {
-                        mStreetViewImage.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                        streetViewImage.getViewTreeObserver().removeOnGlobalLayoutListener(this);
                     } else {
-                        mStreetViewImage.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                        streetViewImage.getViewTreeObserver().removeGlobalOnLayoutListener(this);
                     }
 
                     testLibrary(width);
@@ -73,48 +73,49 @@ public class MainActivity extends Activity {
         Picasso.with(this)
                 .load(getString(R.string.probe_path, width, LAT, LON))
                 .transform(new RoundedCornersTransformation(corner, 0))
-                .into(mStreetViewImage);
+                .into(streetViewImage);
 
-        StreetViewProbe.with(this).probe(LAT, LON, new StreetViewProbe.OnStreetViewStatusListener() {
-            @Override
-            public void onStreetViewStatus(final int status) {
-                MainActivity.this.runOnUiThread(new Runnable() {
+        StreetViewProbe.with(this)
+                .probe(LAT, LON, new StreetViewProbe.OnStreetViewStatusListener() {
                     @Override
-                    public void run() {
-                        switch (status) {
-                            case StreetViewProbe.Status.AVAILABLE:
-                                mTextAvailability.setVisibility(View.VISIBLE);
-                                mDotImage.setVisibility(View.VISIBLE);
-                                mTextAvailability.setText(R.string.available);
-                                mTextAvailability.setSelected(true);
-                                mDotImage.setSelected(true);
-                                break;
-                            case StreetViewProbe.Status.UNAVAILABLE:
-                                mTextAvailability.setVisibility(View.VISIBLE);
-                                mDotImage.setVisibility(View.VISIBLE);
-                                mTextAvailability.setText(R.string.unavailable);
-                                mTextAvailability.setSelected(false);
-                                mDotImage.setSelected(false);
-                                break;
-                            case StreetViewProbe.Status.UNKONWN:
-                                break;
-                        }
+                    public void onStreetViewStatus(@StreetViewProbe.Status final int status) {
+                        MainActivity.this.runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                switch (status) {
+                                    case StreetViewProbe.Status.AVAILABLE:
+                                        textAvailability.setVisibility(View.VISIBLE);
+                                        dotImage.setVisibility(View.VISIBLE);
+                                        textAvailability.setText(R.string.available);
+                                        textAvailability.setSelected(true);
+                                        dotImage.setSelected(true);
+                                        break;
+                                    case StreetViewProbe.Status.UNAVAILABLE:
+                                        textAvailability.setVisibility(View.VISIBLE);
+                                        dotImage.setVisibility(View.VISIBLE);
+                                        textAvailability.setText(R.string.unavailable);
+                                        textAvailability.setSelected(false);
+                                        dotImage.setSelected(false);
+                                        break;
+                                    case StreetViewProbe.Status.UNKNOWN:
+                                        break;
+                                }
+                            }
+                        });
                     }
                 });
-            }
-        });
     }
 
     private void setupViews() {
-        mTextLat = (TextView) findViewById(R.id.text_lat);
-        mTextLon = (TextView) findViewById(R.id.text_lon);
-        mTextAvailability = (TextView) findViewById(R.id.text_avaiability);
-        mStreetViewImage = (ImageView) findViewById(R.id.image_street_view);
-        mDotImage = (ImageView) findViewById(R.id.image_dot);
+        textLat = (TextView) findViewById(R.id.text_lat);
+        textLon = (TextView) findViewById(R.id.text_lon);
+        textAvailability = (TextView) findViewById(R.id.text_avaiability);
+        streetViewImage = (ImageView) findViewById(R.id.image_street_view);
+        dotImage = (ImageView) findViewById(R.id.image_dot);
 
-        mTextLat.setText(getString(R.string.lat, LAT));
-        mTextLon.setText(getString(R.string.lon, LON));
-        mTextAvailability.setVisibility(View.INVISIBLE);
-        mDotImage.setVisibility(View.INVISIBLE);
+        textLat.setText(getString(R.string.lat, LAT));
+        textLon.setText(getString(R.string.lon, LON));
+        textAvailability.setVisibility(View.INVISIBLE);
+        dotImage.setVisibility(View.INVISIBLE);
     }
 }
